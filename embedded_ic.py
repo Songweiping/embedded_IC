19694debdd0b82f1f45d9a1f7ab4ea6703a75e90
songweiping <songweiping@oneniceapp.com>
2016-08-10 23:35:40 +0800

    train func

--- embedded_ic.py
+++ embedded_ic.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 import sys
 import os
 import time
+import math  
 import logging
 import random
 import numpy as np
@@ -21,7 +22,7 @@ flags.DEFINE_string("save_path", None, "path to save the model.")
 flags.DEFINE_integer("max_epoch", 10, "max epochs.")
 flags.DEFINE_integer("emb_dim", 50, "embedding dimension.")
 flags.DEFINE_float("lr", 0.025, "initial learning rate.")
-
+flags.DEFINE_integer("freq", 100, "frequency to output.")
 
 FLAGS = flags.FLAGS
 
@@ -50,6 +51,8 @@ class Options(object):
         #initial learning rate.
         self.lr = FLAGS.lr
 
+        #output frequency.
+        self.freq = FLAGS.freq
 
 class Embedded_IC(object):
     """Embedded IC model."""
@@ -58,6 +61,7 @@ class Embedded_IC(object):
         self._options = options
         self._session = session
         self._u2idx = {}
+        self._idx2u= []
         self._buildIndex()
         self._train_cascades = self._readFromFile(options.train_data)
         self._test_cascades = self._readFromFile(options.test_data)
@@ -97,6 +101,7 @@ class Embedded_IC(object):
         for user in user_set:
             self._u2idx[user] = pos
             pos += 1
+            self._idx2u.append(user)
         opts.user_size = len(user_set)
         logging.info("user_size : %d" % (opts.user_size))
 
@@ -131,35 +136,124 @@ class Embedded_IC(object):
 
         u_emb = tf.nn.embedding_lookup(emb_user, u)
         v_emb = tf.nn.embedding_lookup(emb_user, v)
+        
+        u_0 = tf.slice(u_emb, [0, 0], [1, 1])
+        v_0 = tf.slice(v_emb, [0 ,0], [1, 1])
 
-        u_0 = tf.slice(u_emb, [0], [1])
-        v_0 = tf.slice(v_emb, [0], [1])
-
-        u_1_n = tf.slice(u_emb, [1], [-1])
-        v_1_n = tf.slice(v_emb, [1], [-1])
-
+        u_1_n = tf.slice(u_emb, [0, 1], [1, -1])
+        v_1_n = tf.slice(v_emb, [0, 1], [1, -1])
+        
         x = u_0 + v_0 + tf.reduce_sum(tf.square(tf.sub(u_1_n, v_1_n)))
+        
         f = tf.sigmoid(-x)
+        #one = tf.convert_to_tensor(1.0, dtype = tf.float32)
 
-        one = tf.convert_to_tensor(1.0, dtype = tf.float32)
-
-        loss1 = - tf.mul(tf.sub(one, P_v), tf.log(one - f))
-        loss2 = -tf.mul(P_v, tf.log(f)) + loss1
+        loss1 = - tf.mul(tf.sub(1, P_v), tf.log(1 - f)) - tf.mul(P_v, tf.log(f))
+        loss2 = -tf.log(1.0 - f)
 
         lr = tf.train.exponential_decay(opts.lr, global_step, 1000, 0.96, staircase=True)
 
-        train1 = tf.GradientDescentOptimizer(lr).minimize(loss1, global_step=global_step)
-
-        train2 = tf.GradientDescentOptimizer(lr).minimize(loss2, global_step=global_step)
+        train1 = tf.train.GradientDescentOptimizer(lr).minimize(loss1, global_step=global_step)
+        train2 = tf.train.GradientDescentOptimizer(lr).minimize(loss2, global_step=global_step)
 
         self.u = u
         self.v = v
         self.P_v = P_v
         self.emb_user = emb_user
         self.global_step = global_step
-
+        self.p_uv = f
         self.lr = lr
         self.loss1 = loss1
         self.loss2 = loss2
         self.train1 = train1
         self.train2 = train2
+
+        tf.initialize_all_variables().run()
+        self.saver = tf.train.Saver()
+    
+    def SampleCacade(self):
+        """sample a cascade randomly."""
+        opts = self._options
+        return random.randint(0, opts.user_size - 1)
+        
+
+    def SampleV(self, cascadeId):
+        """sample a user V, which can not be the initial user of the given cascade."""
+        opts = self._options
+        while True:
+            idx = random.randint(0, opts.user_size - 1)
+            if idx == self._train_cascades[cascadeId][0]:
+                continue
+            v_in_cascade = idx in set(self._train_cascades[cascadeId])
+            return v_in_cascade, idx
+
+    def SampleU(self, cascadeId, vId):
+        """sample all users u for sampled cascade and user v."""
+        ulist = []
+        for user in self._train_cascades[cascadeId]:
+            if user == vId:
+                break
+            ulist.append(user)
+        return ulist
+
+    
+    def computePv(self, v, ul):
+        pv = 1.0
+        for u in ul:
+            p_uv = self._session.run(self.p_uv)
+            pv = pv * (1.0 - p_uv)
+        p_v = 1.0 - pv
+        return p_v
+
+    def compute_ll(self):
+        opts = self._options
+        ll = 0.0
+        for i in xrange(opts.train_size):
+            cur = self._train_cascades[i]
+            for j in xrange(1,len(cur)):
+                ll += math.log(self.computePv(cur[j], cur[:j-1]))
+            uset = set(self._train_cascades[i])
+            u_set = set(self._idx2u) - uset
+            for user in u_set:
+                ll += math.log(self.computePv(user, cur))
+
+        return ll
+
+    def train(self):
+        """train the model."""
+        opts = self._options
+        loss_list = []
+        oldL = float("-inf")
+        while True:
+            cascade_id = self.SampleCacade()
+            v_in_cascade, v_id = self.SampleV(cascade_id)
+            u_id_list = self.SampleU(cascade_id, v_id)
+            if v_in_cascade:
+                p_v = self.computePv(v_id, u_id_list)
+                for u in u_id_list:
+                    feed_dict = {self.u:u, self.v:v_id, self.P_v:p_v}
+
+                    (lr, loss, step, _) = self._session.run([self.lr, self.loss1, self.global_step, self.train1], feed_dict=feed_dict)
+            else:
+                for u in u_id_list:
+                    feed_dict = {self.u:u,  self.v:v_id}
+                    (lr, loss, step, _) = self._session.run([self.lr, self.loss2, self.global_step, self.train2], feed_dict=feed_dict)
+            loss_list.append(loss) 
+            if step % opts.freq == 0:
+                L = self.compute_ll()
+                if L < oldL:
+                    break
+
+
+def main(_):
+    logging.basicConfig(level = logging.INFO)
+    #if not FLAGS.train_data:
+    #    logging.error("train file not found.")
+    #    sys.exit(1)
+    options = Options()
+    with tf.Graph().as_default(), tf.Session() as session:
+        model = Embedded_IC(options, session)
+        model.train()
+
+if __name__ ==  "__main__":
+    tf.app.run()
